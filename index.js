0427ab1c0f802b9532f7dbbef5bd1be6fe1d51bf
gaumeister <derek@apigrate.com>
2019-07-10 14:35:00 -0500

    Apache License 2.0

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/*
+  Copyright 2019 Apigrate LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 const debug        = require('debug')('gr8:tradegecko');
 const request      = require('request-promise-native');
 /**
@@ -276,7 +291,7 @@ class TradeGecko{
       this.rate_limit_remaining  = resp.headers['x-rate-limit-remaining'];
       this.rate_limit_reset      = resp.headers['x-rate-limit-reset'];
       this.request_id            = resp.headers['x-request-id'];
-      
+
       return JSON.parse(resp.body);
     } else {
       //Errors.
